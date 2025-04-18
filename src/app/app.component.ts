c07224fb11a42cb317e22c9abb7e989d9d45bc7a
miro <miro@localhost.localdomain>
2025-04-18 19:33:53 +0300

    Basic usage of new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Country } from './api/models';
 import { CountryService } from './api/services';
+import { SiteInfoComponent } from "./feature/site-info/site-info.component";
+import { ToolbarComponent } from "./feature/toolbar/toolbar.component";
+import { DisplayContentComponent } from "./feature/display-content/display-content.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SiteInfoComponent, ToolbarComponent, DisplayContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
