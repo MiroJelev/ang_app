1e83e5e2f908816e15789c4822ffeced80fbee37
miro <miro@localhost.localdomain>
2025-04-18 19:33:03 +0300

    Display content component

---
+++ src/app/feature/display-content/display-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './display-content.component.html',
+  styleUrl: './display-content.component.css'
+})
+export class DisplayContentComponent {
+
+}
