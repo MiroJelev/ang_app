1eb89c1cb14db0c38e08a4b9443a5e6bbc1a3bcf
miro <miro@localhost.localdomain>
2025-04-21 07:41:22 +0300

    Update details view

--- src/app/feature/details-view/details-view.component.ts
+++ src/app/feature/details-view/details-view.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Country } from '../../api/models';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-details-view',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule, MatListModule],
   templateUrl: './details-view.component.html',
   styleUrl: './details-view.component.css'
 })
 export class DetailsViewComponent {
-
+  @Input() country?: Country;
 }
